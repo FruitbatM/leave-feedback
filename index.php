e34d4f39b65e3edf76b34354025dfe3ffaa94864
marinaP <mar.pavlovic@gmail.com>
2022-07-02 22:47:24 +0100

    Refactor index file and add includes folder with header and footer files

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'includes/header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Leave Feedback</title>
-</head>
+<img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
+<h2>Feedback</h2>
+<p class="lead text-center">Leave feedback for Lollipop Media</p>
+<form action="" class="mt-4 w-75">
+  <div class="mb-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+  </div>
+  <div class="mb-3">
+    <label for="body" class="form-label">Feedback</label>
+    <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+  </div>
+  <div class="mb-3">
+    <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
+  </div>
+</form>
 
-<body>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
-</body>
-
-</html>
+<?php include 'includes/footer.php'; ?>
